033646560a7d893f56ab56b812e40e24be3a574e
JooJ <joaopedroacostaalvarez@gmail.com>
2025-09-19 19:14:49 -0300

    Maven e MySQL

---
+++ src/main/java/application/Program.java
@@ -0,0 +1,28 @@
+package application;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import domain.Person;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("estudos-jpa");
+		
+		EntityManager em = emf.createEntityManager();
+
+		Person p = em.find(Person.class, 1);
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		System.out.println("Pronto");
+		
+		em.close();
+		emf.close();
+		
+	}
+
+}
